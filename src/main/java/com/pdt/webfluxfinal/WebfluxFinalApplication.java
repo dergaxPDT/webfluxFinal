0edacf56b0aced31a5f6c751ab6db24040e9f500
Dergax <dergax.s3@gmail.com>
2023-03-16 22:50:31 +0100

    Initial commit

---
+++ src/main/java/com/pdt/webfluxfinal/WebfluxFinalApplication.java
@@ -0,0 +1,13 @@
+package com.pdt.webfluxfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pdt/webfluxfinal/WebfluxFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdt.webfluxfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
