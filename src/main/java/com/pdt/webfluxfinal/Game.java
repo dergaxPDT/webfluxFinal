0f3d14ac87db3267d11580143073666ec36fc1f2
DergaxPDT <dergax.s3@gmail.com>
2023-03-18 16:01:37 +0100

    i

--- src/main/java/com/pdt/webfluxfinal/Game.java
+++ src/main/java/com/pdt/webfluxfinal/Game.java
@@ -6,4 +6,12 @@ public class Game {
     public Game(String name) {
         this.name = name;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/test/java/com/pdt/webfluxfinal/WebfluxFinalApplicationTests.java
+++ src/test/java/com/pdt/webfluxfinal/WebfluxFinalApplicationTests.java
@@ -1,26 +1,49 @@
 package com.pdt.webfluxfinal;
 
 import io.restassured.RestAssured;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import io.restassured.RestAssured.*;
-        import io.restassured.matcher.RestAssuredMatchers.*;
-        import org.hamcrest.Matchers.*;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class WebfluxFinalApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
+    //    @Before
+//    public void setup(){
+//        RestAssured.baseURI = "https://api.github.com";
+//        RestAssured.port = 443;
+//    }
+    private String uri;
+    @LocalServerPort
+    private int port;
+
+    @PostConstruct
+    public void init() {
+        uri = "http://localhost:" + port;
+    }
+
     @Test
-    void saveAndGetList(){
+    void saveAndGetList() {
         Game game = new Game("cs:go");
+        Map<String, String> request = new HashMap();
+        request.put("name", game.getName());
         RestAssured
                 .given()
-                .body(game)
+                .body(request)
                 .when()
-                .post("api/game")
+                .post(uri + "api/game")
                 .then()
                 .statusCode(200);
 
