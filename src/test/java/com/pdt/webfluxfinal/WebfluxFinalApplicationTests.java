49715d188e7d5c96a7d1717ac3e1e6db72d5f707
Dergax <dergax.s3@gmail.com>
2023-03-21 19:01:40 +0100

    web flux worked version

--- src/test/java/com/pdt/webfluxfinal/WebfluxFinalApplicationTests.java
+++ src/test/java/com/pdt/webfluxfinal/WebfluxFinalApplicationTests.java
@@ -7,35 +7,29 @@ import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("default")
 class WebfluxFinalApplicationTests {
 
     @LocalServerPort
     private int port;
 
-//    @Before
-//    public void setup() {
-//        RestAssured.baseURI = "http://localhost";
-//        RestAssured.port = port;
-//    }
-
     @Test
     void contextLoads() {
     }
-    static {
-        System.setProperty("spring.mongodb.embedded.version","5.0.0");
-    }
+//    static {
+//        System.setProperty("spring.mongodb.embedded.version","5.0.0");
+//    }
     @Test
     void saveAndGetList(){
-        System.out.println("@@"+RestAssured.baseURI);
-        System.out.println("@@"+RestAssured.port);
         Game game = new Game("cs:go");
         RestAssured
                 .given()
                 .port(port)
-                .body(game)                .contentType(ContentType.JSON)
-
+                .body(game)
+                .contentType(ContentType.JSON)
                 .when()
                 .post("/api/game")
                 .then()
